e7616ad6a7f52cca377b4ab14666d54bd05ad813
tradervicky <vickygupta031@gmail.com>
2024-01-05 00:06:01 +0530

    one player logic completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Board from './components/Board';
+
 
 function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xPlaying, setXplaying] = useState(true)
+  const handleBoxClick = (boxIdx)=>{
+    // step 1 update the board
+    const updatedBoard = board.map((value, idx)=>{
+      
+      if(idx === boxIdx){
+        return xPlaying === true ? "X" :"O";
+      }
+      else{
+        return value;
+      }
+    })
+    setBoard(updatedBoard)
+    setXplaying(!xPlaying)
+  }
+  
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Board board={board} onClick={handleBoxClick}/>
+    
     </div>
   );
 }
---
+++ src/components/Board.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Board.css'
+import Box from './Box'
+const Board = ({board, onClick}) => {
+  return (
+    <div className='board'>
+        {board.map((data, idx)=>
+        {
+            return <Box key={idx} value={data} onClick={()=>onClick(idx)}/>
+        }
+        
+        
+        )}
+    </div>
+  )
+}
+
+export default Board
